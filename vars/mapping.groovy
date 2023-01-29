256dde45d29c4c837209898bce9d3bb8df80621b
toan la <ldtoan1306@gmail.com>
2023-01-29 16:47:33 +0700

    add load resource in sharedlibs

---
+++ vars/mapping.groovy
@@ -0,0 +1,3 @@
+def call(Map config=[:]){
+    echo "Hello ${config.name}. Today is ${config.dayOfWeek}."
+}
--- vars/test.groovy
+++
@@ -1,6 +0,0 @@
-def call1(name){
-    echo "Hey ${name}, how are you?"
-}
-def call2(Map config=[:]){
-    echo "Hello ${config.name}. Today is ${config.dayOfWeek}."
-}
