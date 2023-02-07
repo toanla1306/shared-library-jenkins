9cfe474766b08cebe324486e545a5a0114c82bbd
toan la <ldtoan1306@gmail.com>
2023-02-07 11:37:15 +0700

    test

--- vars/helloworldexternal.groovy
+++ vars/helloworldexternal.groovy
@@ -1,4 +1,8 @@
 def call(Map config = [:]) {
-    loadLinuxScript(name: 'hello-world.sh')
-    sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+    stage('helloworld') {
+        timeout (time: 30, unit: 'MINUTES'){
+            loadLinuxScript(name: 'hello-world.sh')
+            sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+        }
+    }
 }
